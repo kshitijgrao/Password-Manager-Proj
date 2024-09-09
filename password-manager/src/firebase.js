a37958da6f4753353a102eb7ceb8bbdf43e8b021
Kshitij Rao <kshitijgrao@gmail.com>
2024-09-09 03:57:43 -0700

    created source files for project

---
+++ password-manager/src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDot-qLrGIUWT128ISJ_6inz-PCPqWTRaQ",
+    authDomain: "password-manager-c8777.firebaseapp.com",
+    projectId: "password-manager-c8777",
+    storageBucket: "password-manager-c8777.appspot.com",
+    messagingSenderId: "861504170376",
+    appId: "1:861504170376:web:0ecd9f00b853e28968323d",
+    measurementId: "G-D8MC8MDQ6P"
+};  
+  // Initialize Firebase
+export const app = initializeApp(firebaseConfig);
